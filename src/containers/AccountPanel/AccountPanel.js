1631a276df64924c6b4491ea92329a649ede043e
Jean-Frederic Mainville <jfmainville@outlook.com>
2018-10-16 16:30:54 -0400

    Add the AccountPanel component to the project

---
+++ src/containers/AccountPanel/AccountPanel.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+
+class AccountPanel extends Component {
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Account Panel</p>
+            </div>
+        )
+    }
+}
+
+export default AccountPanel;
