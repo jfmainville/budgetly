9dc7c317fc4f75b80a8b548875d54c94a59263b5
Jean-Frederic Mainville <jfmainville@outlook.com>
2018-10-16 16:31:06 -0400

    Add the DashboardPanel component to the project

---
+++ src/containers/DashboardPanel/DashboardPanel.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+
+class DashboardPanel extends Component {
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Dashboard Panel</p>
+            </div>
+        )
+    }
+}
+
+export default DashboardPanel;
