6f9edf8e2ff84fbe97d2e71699342bd8a970d6bc
Jean-Frederic Mainville <jfmainville@outlook.com>
2018-10-16 16:31:40 -0400

    Add the TransactionPanel component to the project

---
+++ src/containers/TransactionPanel/TransactionPanel.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+
+class TransactionPanel extends Component {
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Transaction Panel</p>
+            </div>
+        )
+    }
+}
+
+export default TransactionPanel;
