b279ba9712e40f5294f57e1945553c0df0854191
Jean-Frederic Mainville <jfmainville@outlook.com>
2018-10-18 13:10:13 -0400

    Add the AccountCard component to the project

---
+++ src/components/AccountTable/AccountCard/AccountCard.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import classes from "./AccountCard.module.scss";
+
+
+const accountCard = (props) => {
+    const account = props.account;
+    const totalAmount = new Intl.NumberFormat('en-CA', {style: 'currency', currency: 'CAD'}).format(account.total);
+    return (
+        <div className={classes.TableRow}>
+            <div className={classes.TableSelectRow}>
+                <input
+                    className={classes.TableCheckboxInput}
+                    type="checkbox"
+                />
+            </div>
+            <div className={classes.TableDateRow}>
+                {account.date}
+            </div>
+            <div className={classes.TableEnterpriseRow}>
+                {account.enterprise}
+            </div>
+            <div className={classes.TableCategoryRow}>
+                {account.category}
+            </div>
+            <div className={classes.TableTotalRow}>
+                {totalAmount}
+            </div>
+        </div>
+    )
+};
+
+export default accountCard;
