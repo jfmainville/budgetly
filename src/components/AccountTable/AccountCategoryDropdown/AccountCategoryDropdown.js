b9272acf4634ee4359588c704b4fede41e48bebe
Jean-Frederic Mainville <jfmainville@outlook.com>
2018-10-24 17:07:32 -0400

    Remove the Container class from the AccountCategoryDropdown component

--- src/components/AccountTable/AccountCategoryDropdown/AccountCategoryDropdown.js
+++ src/components/AccountTable/AccountCategoryDropdown/AccountCategoryDropdown.js
@@ -7,82 +7,65 @@ import {Manager, Reference, Popper} from 'react-popper';
 const accountCategoryDropdown = (props) => {
     const categories = props.categories;
     return (
-        <div className={classes.Container}>
-            <Manager>
-                <div className={classes.CategoryDropdown}>
-                    <Reference>
-                        {({ref}) => (
-                            <button
+        <Manager>
+            <div className={classes.CategoryDropdown}>
+                <Reference>
+                    {({ref}) => (
+                        <button
+                            ref={ref}
+                            className={classes.CategoryDropdownButton}
+                            onClick={props.handleShowCategoryDropdown}
+                        >
+                            <svg className={classes.CategoryDropdownButtonIcon}>
+                                <use
+                                    xlinkHref="/assets/sprite.svg#icon-chevron-down"/>
+                            </svg>
+                        </button>
+                    )}
+                </Reference>
+                <Reference>
+                    {({ref}) => (
+                        <div className={classes.CategoryDropdown}>
+                            <input
                                 ref={ref}
-                                className={classes.CategoryDropdownButton}
-                                onClick={props.handleShowCategoryDropdown}
+                                className={classes.CategoryDropdownInput}
+                                type="text"
+                                placeholder="Category"
+                                value={props.categorySearchInput}
+                                onChange={props.handleCategorySearch}
+                            />
+                            <button
+                                className={classes.CategoryDropdownClearButton}
+                                onClick={props.handleClearCategorySearch}
                             >
-                                <svg className={classes.CategoryDropdownButtonIcon}>
-                                    <use
-                                        xlinkHref="/assets/sprite.svg#icon-chevron-down"/>
-                                </svg>
+                                X
                             </button>
-                        )}
-                    </Reference>
-                    <Reference>
-                        {({ref}) => (
-                            <div className={classes.CategoryDropdown}>
-                                <input
+                        </div>
+                    )}
+                </Reference>
+                {props.showCategoryDropdown ?
+                    <div className={classes.TransparentBackgroundWrapper}>
+                        <div
+                            onClick={props.handleShowCategoryDropdown}
+                            className={classes.TransparentBackground}>
+                        </div>
+                        <Popper
+                            placement="bottom"
+                            positionFixed="true"
+                        >
+                            {({ref, style, placement}) => (
+                                <div
                                     ref={ref}
-                                    className={classes.CategoryDropdownInput}
-                                    type="text"
-                                    placeholder="Category"
-                                    value={props.categorySearchInput}
-                                    onChange={props.handleCategorySearch}
-                                />
-                                <button
-                                    className={classes.CategoryDropdownClearButton}
-                                    onClick={props.handleClearCategorySearch}
+                                    style={style}
+                                    data-placement={placement}
+                                    className={classes.ShowCategoryDropdown}
                                 >
-                                    X
-                                </button>
-                            </div>
-                        )}
-                    </Reference>
-                    {props.showCategoryDropdown ?
-                        <div className={classes.TransparentBackgroundWrapper}>
-                            <div
-                                onClick={props.handleShowCategoryDropdown}
-                                className={classes.TransparentBackground}>
-                            </div>
-                            <Popper
-                                placement="bottom"
-                                positionFixed="true"
-                            >
-                                {({ref, style, placement}) => (
-                                    <div
-                                        ref={ref}
-                                        style={style}
-                                        data-placement={placement}
-                                        className={classes.ShowCategoryDropdown}
-                                    >
-                                        {categories.map(category => {
-                                                if (props.categorySearchInput !== "") {
-                                                    const regex = new RegExp("(" + props.categorySearchInput + ")", "gi");
-                                                    const match = category.title.match(regex);
-                                                    if (match != null) {
-                                                        let parts = category.title.split(match[0], 2);
-                                                        return (
-                                                            <div
-                                                                className={classes.ShowCategoryDropdownRow}
-                                                                key={category.id}
-                                                                onClick={() => props.handleCategorySearchSelection(category)}
-                                                            >
-                                                                <p className={classes.ShowCategoryDropdownTitle}>
-                                                                    {parts[0]}
-                                                                    <strong>{match[0]}</strong>
-                                                                    {parts[1]}{" "}
-                                                                </p>
-                                                            </div>
-                                                        );
-                                                    }
-                                                }
-                                                else {
+                                    {categories.map(category => {
+                                            if (props.categorySearchInput !== "") {
+                                                const regex = new RegExp("(" + props.categorySearchInput + ")", "gi");
+                                                const match = category.title.match(regex);
+                                                if (match != null) {
+                                                    let parts = category.title.split(match[0], 2);
                                                     return (
                                                         <div
                                                             className={classes.ShowCategoryDropdownRow}
@@ -90,24 +73,39 @@ const accountCategoryDropdown = (props) => {
                                                             onClick={() => props.handleCategorySearchSelection(category)}
                                                         >
                                                             <p className={classes.ShowCategoryDropdownTitle}>
-                                                                {category.title}
+                                                                {parts[0]}
+                                                                <strong>{match[0]}</strong>
+                                                                {parts[1]}{" "}
                                                             </p>
                                                         </div>
-                                                    )
+                                                    );
                                                 }
-                                                return null
                                             }
-                                        )}
-                                    </div>
-                                )}
-                            </Popper>
-                        </div>
-                        :
-                        null
-                    }
-                </div>
-            </Manager>
-        </div>
+                                            else {
+                                                return (
+                                                    <div
+                                                        className={classes.ShowCategoryDropdownRow}
+                                                        key={category.id}
+                                                        onClick={() => props.handleCategorySearchSelection(category)}
+                                                    >
+                                                        <p className={classes.ShowCategoryDropdownTitle}>
+                                                            {category.title}
+                                                        </p>
+                                                    </div>
+                                                )
+                                            }
+                                            return null
+                                        }
+                                    )}
+                                </div>
+                            )}
+                        </Popper>
+                    </div>
+                    :
+                    null
+                }
+            </div>
+        </Manager>
     )
 };
 
