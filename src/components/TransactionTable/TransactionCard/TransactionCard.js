27c0b9f956b928236d184db74a6ee5f15188275f
Jean-Frederic Mainville <jfmainville@outlook.com>
2018-10-26 15:28:59 -0400

    Add the TransactionCard component to the project

---
+++ src/components/TransactionTable/TransactionCard/TransactionCard.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import classes from "./TransactionCard.module.scss";
+import PropTypes from 'prop-types';
+
+
+const transactionCard = (props) => {
+    const transaction = props.transaction;
+    const totalAmount = new Intl.NumberFormat('en-CA', {style: 'currency', currency: 'CAD'}).format(transaction.total);
+    return (
+        <div className={classes.TableRow}>
+            <div className={classes.TableSelectRow}>
+                <input
+                    className={classes.TableCheckboxInput}
+                    type="checkbox"
+                />
+            </div>
+            <div className={classes.TableEnterpriseRow}>
+                {transaction.date}
+            </div>
+            <div className={classes.TableEnterpriseRow}>
+                {transaction.enterprise}
+            </div>
+            <div className={classes.TableTypeRow}>
+                {transaction.type}
+            </div>
+            <div className={classes.TableCategoryRow}>
+                {transaction.category}
+            </div>
+            <div className={classes.TableTotalRow}>
+                {totalAmount}
+            </div>
+        </div>
+    )
+};
+
+transactionCard.propTypes = {
+    transaction: PropTypes.object,
+    totalAmount: PropTypes.number
+};
+
+export default transactionCard;
---
+++ src/components/TransactionTable/TransactionCard/TransactionCard.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {configure, mount} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import TransactionCard from "./TransactionCard";
+
+
+configure({adapter: new Adapter()});
+
+describe('<TransactionCard/>', () => {
+    const props = {
+        transaction: {id: 1, enterprise: "Tim Hortons", type: "Expense", category: "Restaurant", total: 920.24},
+    };
+    const location = {pathname: '/accounts'};
+    let wrapper;
+    beforeEach(() => {
+        wrapper = mount(<TransactionCard {...props} location={location}/>);
+    });
+
+    it('should have one transaction prop object', () => {
+        expect(wrapper.props().transaction).toBeDefined();
+    });
+
+    afterEach(() => {
+        wrapper.unmount();
+    })
+});
+
