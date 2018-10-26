4316f4d15c9a975b756a9b4858b1207cd886e2eb
Jean-Frederic Mainville <jfmainville@outlook.com>
2018-10-26 15:27:13 -0400

    Add the handleMonthSelectionPrevious, handleMonthSelectionNext functions to the TransactionPanel component
    - Add the transactions and activeMonth to the initial state
    - Add the transactions and activeMonth to the initial state in the TransactionTable component
    - Add the handleMonthSelectionPrevious, handleMonthSelectionNext functions to the TransactionTable component

--- src/containers/TransactionPanel/TransactionPanel.js
+++ src/containers/TransactionPanel/TransactionPanel.js
@@ -6,6 +6,60 @@ import TransactionTable from "../../components/TransactionTable/TransactionTable
 
 
 class TransactionPanel extends Component {
+    state = {
+        transactions: [
+            {
+                id: 1,
+                date: "2018-10-11",
+                enterprise: "Tim Hortons",
+                type: "Expense",
+                category: "Restaurant",
+                total: 1000.24
+            },
+            {
+                id: 2,
+                date: "2018-09-11",
+                enterprise: "McDonalds",
+                type: "Expense",
+                category: "Restaurant",
+                total: 1000.24
+            },
+            {
+                id: 3,
+                date: "2018-08-11",
+                enterprise: "Burger King",
+                type: "Expense",
+                category: "Restaurant",
+                total: 1000.24
+            },
+            {
+                id: 4,
+                date: "2018-10-11",
+                enterprise: "Netflix",
+                type: "Expense",
+                category: "Entertainment",
+                total: 1000.24
+            },
+            {id: 5, date: "2018-10-11", enterprise: "Metro", type: "Expense", category: "Grocery", total: 1000.24},
+            {id: 6, date: "2018-10-11", enterprise: "IGA", type: "Expense", category: "Grocery", total: 1000.24},
+        ],
+        activeMonth: moment().format("YYYY-MM")
+    };
+
+    handleMonthSelectionPrevious = () => {
+        let activeMonth = moment(this.state.activeMonth, "YYYY-MM").add(-1, "month").format("YYYY-MM");
+        this.setState({
+            activeMonth: activeMonth
+        })
+    };
+
+    handleMonthSelectionNext = () => {
+        let activeMonth = moment(this.state.activeMonth, "YYYY-MM").add(1, "month").format("YYYY-MM");
+        this.setState({
+            activeMonth: activeMonth
+        })
+    };
+
     componentDidMount() {
 
     }
