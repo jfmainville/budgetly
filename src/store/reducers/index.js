695e528fa519f0f772a2e46ae0a669aa20f254be
Jean-Frederic Mainville <jfmainville@outlook.com>
2018-10-29 15:23:33 -0400

    Add the accounts and transactions reducers to the reducers index.js file

---
+++ src/store/reducers/index.js
@@ -0,0 +1,9 @@
+import {combineReducers} from 'redux';
+import accountReducer from "./accountReducer";
+import transactionReducer from "./transactionReducer";
+
+
+export default combineReducers({
+    account: accountReducer,
+    transaction: transactionReducer,
+});
