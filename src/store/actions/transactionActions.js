f5e603c282dcbf993449a983d6fb1cb4eb9f521b
Jean-Frederic Mainville <jfmainville@outlook.com>
2018-10-29 15:15:15 -0400

    Add the transactions actions creator to the transactionActions actions creator

---
+++ src/store/actions/transactionActions.js
@@ -0,0 +1,153 @@
+import * as actionTypes from './actionTypes';
+
+
+export function fetchTransactionsLoad(bool) {
+    return {
+        type: actionTypes.FETCH_TRANSACTIONS_LOAD,
+        loading: bool
+    }
+}
+
+export function fetchTransactionsSuccess(data) {
+    return {
+        type: actionTypes.FETCH_TRANSACTIONS_SUCCESS,
+        transactions: data
+    }
+}
+
+export function fetchTransactionsFail(bool) {
+    return {
+        type: actionTypes.FETCH_TRANSACTIONS_FAIL,
+        error: bool
+    }
+}
+
+export function createTransactionLoad(bool) {
+    return {
+        type: actionTypes.CREATE_TRANSACTION_LOAD,
+        loading: bool
+    }
+}
+
+export function createTransactionSuccess(data) {
+    return {
+        type: actionTypes.CREATE_TRANSACTION_SUCCESS,
+        createTransaction: data
+    }
+}
+
+export function createTransactionFail(bool) {
+    return {
+        type: actionTypes.CREATE_TRANSACTION_FAIL,
+        error: bool
+    }
+}
+
+export function updateTransactionLoad(bool) {
+    return {
+        type: actionTypes.UPDATE_TRANSACTION_LOAD,
+        loading: bool
+    }
+}
+
+export function updateTransactionSuccess(data) {
+    return {
+        type: actionTypes.UPDATE_TRANSACTION_SUCCESS,
+        updateTransaction: data
+    }
+}
+
+export function updateTransactionFail(bool) {
+    return {
+        type: actionTypes.UPDATE_TRANSACTION_FAIL,
+        error: bool
+    }
+}
+
+export function deleteTransactionLoad(bool) {
+    return {
+        type: actionTypes.DELETE_TRANSACTION_LOAD,
+        loading: bool
+    }
+}
+
+export function deleteTransactionSuccess(data) {
+    return {
+        type: actionTypes.DELETE_TRANSACTION_SUCCESS,
+        deleteTransaction: data
+    }
+}
+
+export function deleteTransactionFail(bool) {
+    return {
+        type: actionTypes.DELETE_TRANSACTION_FAIL,
+        error: bool
+    }
+}
+
+export function createTransactionRowLoad(bool) {
+    return {
+        type: actionTypes.CREATE_TRANSACTION_ROW_LOAD,
+        loading: bool
+    }
+}
+
+export function createTransactionRowSuccess(rowId, transactionId) {
+    return {
+        type: actionTypes.CREATE_TRANSACTION_ROW_SUCCESS,
+        rowId: rowId,
+        transactionId: transactionId
+    }
+}
+
+export function deleteTransactionRowLoad(bool) {
+    return {
+        type: actionTypes.DELETE_TRANSACTION_ROW_LOAD,
+        loading: bool
+    }
+}
+
+export function deleteTransactionRowSuccess(rowId, transactionId) {
+    return {
+        type: actionTypes.DELETE_TRANSACTION_ROW_SUCCESS,
+        rowId: rowId,
+        transactionId: transactionId,
+    }
+}
+
+
+export function fetchTransactions(data) {
+    return (dispatch) => {
+        dispatch(fetchTransactionsSuccess(data));
+    }
+}
+
+export function updateTransaction(data) {
+    return (dispatch) => {
+        dispatch(updateTransactionSuccess(data));
+    };
+}
+
+export function deleteTransaction(data) {
+    return (dispatch) => {
+        dispatch(deleteTransactionSuccess(data));
+    };
+}
+
+export function createTransaction(data) {
+    return (dispatch) => {
+        dispatch(createTransactionSuccess(data));
+    };
+}
+
+export function createTransactionRow(rowId, transactionId) {
+    return dispatch => {
+        dispatch(createTransactionRowSuccess(rowId, transactionId));
+    }
+}
+
+export function deleteTransactionRow(rowId, transactionId) {
+    return dispatch => {
+        dispatch(deleteTransactionRowSuccess(rowId, transactionId));
+    }
+}
---
+++ src/store/actions/transactionActions.test.js
@@ -0,0 +1,139 @@
+import * as actions from './transactionActions';
+import * as actionTypes from './actionTypes';
+
+
+describe('transactionActions', () => {
+    it('should execute the fetchTransactionsLoad action', () => {
+        const bool = true;
+        const expectedAction = {
+            type: actionTypes.FETCH_TRANSACTIONS_LOAD,
+            loading: bool
+        };
+        expect(actions.fetchTransactionsLoad(bool)).toEqual(expectedAction)
+    });
+    it('should execute the fetchTransactionsSuccess action', () => {
+        const data = 'transactions';
+        const expectedAction = {
+            type: actionTypes.FETCH_TRANSACTIONS_SUCCESS,
+            transactions: data
+        };
+        expect(actions.fetchTransactionsSuccess(data)).toEqual(expectedAction)
+    });
+    it('should execute the fetchTransactionsFail action', () => {
+        const bool = true;
+        const expectedAction = {
+            type: actionTypes.FETCH_TRANSACTIONS_FAIL,
+            error: bool
+        };
+        expect(actions.fetchTransactionsFail(bool)).toEqual(expectedAction)
+    });
+    it('should execute the createTransactionLoad action', () => {
+        const bool = true;
+        const expectedAction = {
+            type: actionTypes.CREATE_TRANSACTION_LOAD,
+            loading: bool
+        };
+        expect(actions.createTransactionLoad(bool)).toEqual(expectedAction)
+    });
+    it('should execute the createTransactionSuccess action', () => {
+        const data = 'transactions';
+        const expectedAction = {
+            type: actionTypes.CREATE_TRANSACTION_SUCCESS,
+            createTransaction: data
+        };
+        expect(actions.createTransactionSuccess(data)).toEqual(expectedAction)
+    });
+    it('should execute the createTransactionFail action', () => {
+        const bool = true;
+        const expectedAction = {
+            type: actionTypes.CREATE_TRANSACTION_FAIL,
+            error: bool
+        };
+        expect(actions.createTransactionFail(bool)).toEqual(expectedAction)
+    });
+    it('should execute the updateTransactionLoad action', () => {
+        const bool = true;
+        const expectedAction = {
+            type: actionTypes.UPDATE_TRANSACTION_LOAD,
+            loading: bool
+        };
+        expect(actions.updateTransactionLoad(bool)).toEqual(expectedAction)
+    });
+    it('should execute the updateTransactionSuccess action', () => {
+        const data = 'transactions';
+        const expectedAction = {
+            type: actionTypes.UPDATE_TRANSACTION_SUCCESS,
+            updateTransaction: data
+        };
+        expect(actions.updateTransactionSuccess(data)).toEqual(expectedAction)
+    });
+    it('should execute the updateTransactionFail action', () => {
+        const bool = true;
+        const expectedAction = {
+            type: actionTypes.UPDATE_TRANSACTION_FAIL,
+            error: bool
+        };
+        expect(actions.updateTransactionFail(bool)).toEqual(expectedAction)
+    });
+    it('should execute the deleteTransactionLoad action', () => {
+        const bool = true;
+        const expectedAction = {
+            type: actionTypes.DELETE_TRANSACTION_LOAD,
+            loading: bool
+        };
+        expect(actions.deleteTransactionLoad(bool)).toEqual(expectedAction)
+    });
+    it('should execute the deleteTransactionSuccess action', () => {
+        const data = 'transactions';
+        const expectedAction = {
+            type: actionTypes.DELETE_TRANSACTION_SUCCESS,
+            deleteTransaction: data
+        };
+        expect(actions.deleteTransactionSuccess(data)).toEqual(expectedAction)
+    });
+    it('should execute the deleteTransactionFail action', () => {
+        const bool = true;
+        const expectedAction = {
+            type: actionTypes.DELETE_TRANSACTION_FAIL,
+            error: bool
+        };
+        expect(actions.deleteTransactionFail(bool)).toEqual(expectedAction)
+    });
+    it('should execute the createTransactionRowLoad action', () => {
+        const bool = true;
+        const expectedAction = {
+            type: actionTypes.CREATE_TRANSACTION_ROW_LOAD,
+            loading: bool
+        };
+        expect(actions.createTransactionRowLoad(bool)).toEqual(expectedAction)
+    });
+    it('should execute the createTransactionRowSuccess action', () => {
+        const rowId = 1;
+        const transactionId = 1;
+        const expectedAction = {
+            type: actionTypes.CREATE_TRANSACTION_ROW_SUCCESS,
+            rowId: rowId,
+            transactionId: transactionId
+        };
+        expect(actions.createTransactionRowSuccess(rowId, transactionId)).toEqual(expectedAction)
+    });
+    it('should execute the deleteTransactionRowLoad action', () => {
+        const bool = true;
+        const expectedAction = {
+            type: actionTypes.DELETE_TRANSACTION_ROW_LOAD,
+            loading: bool
+        };
+        expect(actions.deleteTransactionRowLoad(bool)).toEqual(expectedAction)
+    });
+    it('should execute the deleteTransactionRowSuccess action', () => {
+        const rowId = 1;
+        const transactionId = 1;
+        const expectedAction = {
+            type: actionTypes.DELETE_TRANSACTION_ROW_SUCCESS,
+            rowId: rowId,
+            transactionId: transactionId
+        };
+        expect(actions.deleteTransactionRowSuccess(rowId, transactionId)).toEqual(expectedAction)
+    });
+});
+
