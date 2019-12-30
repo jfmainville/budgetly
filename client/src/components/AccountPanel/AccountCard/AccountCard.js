bcaed1a83d20c09e96d59ed521964580a4d6b96a
Jean-Frederic Mainville <jfmainville@outlook.com>
2019-12-30 13:34:09 -0500

    Add the totalFilteredTransactions variable
    Add the totalFilteredTransactions variable to extract the total amount for each account depending on the transactions total.

--- client/src/components/AccountPanel/AccountCard/AccountCard.js
+++ client/src/components/AccountPanel/AccountCard/AccountCard.js
@@ -3,10 +3,24 @@ import classes from "./AccountCard.module.scss";
 
 const accountCard = props => {
 	const account = props.account;
-	const totalAmount = new Intl.NumberFormat("en-CA", {
-		style: "currency",
-		currency: "CAD"
-	}).format(account.total);
+	const transactions = props.transactions;
+	let filteredTransactions = [];
+	let totalFilteredTransactions = [];
+	if (transactions) {
+		filteredTransactions = transactions.filter(
+			transaction => transaction.enterprise === account.enterprise
+		);
+		if (filteredTransactions.length > 0) {
+			totalFilteredTransactions = filteredTransactions
+				.map(transaction => transaction.total)
+				.reduce((accumulator, currentValue) => accumulator + currentValue);
+			totalFilteredTransactions = new Intl.NumberFormat("en-CA", {
+				style: "currency",
+				currency: "CAD"
+			}).format(totalFilteredTransactions);
+		}
+	}
+
 	return (
 		<div className={classes.TableRow}>
 			<div className={classes.TableSelectRow}>
@@ -15,7 +29,7 @@ const accountCard = props => {
 			<div className={classes.TableEnterpriseRow}>{account.enterprise}</div>
 			<div className={classes.TableTypeRow}>{account.type}</div>
 			<div className={classes.TableCategoryRow}>{account.category}</div>
-			<div className={classes.TableTotalRow}>{totalAmount}</div>
+			<div className={classes.TableTotalRow}>{totalFilteredTransactions}</div>
 		</div>
 	);
 };
