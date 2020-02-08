19ba89df506ed2717ba15c09afea2e52220402b8
Jean-Frederic Mainville <jfmainville@outlook.com>
2020-02-08 10:18:37 -0500

    Update the dummy transaction data dates
    Update the dummy transaction data dates to make it easier to analyze the date on the page.
    
    - Update the dummy transaction data dates
    - Format the DummyData.js file to comply with the linting standards

--- client/src/utils/DummyData.js
+++ client/src/utils/DummyData.js
@@ -1,216 +1,216 @@
 export const dummyAccounts = [
-    {
-        id: 1,
-        enterprise: "Tim Hortons",
-        type: "Expense",
-        category: "Restaurant",
-    },
-    {
-        id: 2,
-        enterprise: "McDonalds",
-        type: "Expense",
-        category: "Restaurant",
-    },
-    {
-        id: 3,
-        enterprise: "Burger King",
-        type: "Expense",
-        category: "Restaurant",
-    },
-    {
-        id: 4,
-        enterprise: "Netflix",
-        type: "Expense",
-        category: "Entertainment",
-    },
-    {
-        id: 5,
-        enterprise: "Metro",
-        type: "Expense",
-        category: "Grocery",
-    },
-    {
-        id: 6,
-        enterprise: "IGA",
-        type: "Expense",
-        category: "Grocery",
-    },
-    {
-        id: 7,
-        enterprise: "Pacini",
-        type: "Expense",
-        category: "Restaurant",
-    },
-    {
-        id: 8,
-        enterprise: "Cineplex Odeon",
-        type: "Expense",
-        category: "Entertainment",
-    },
-    {
-        id: 9,
-        enterprise: "Desjardins",
-        type: "Expense",
-        category: "Bank",
-    },
-    {
-        id: 10,
-        enterprise: "Services Conseils Franc-Jeu",
-        type: "Income",
-        category: "Revenue",
-    },
-    {
-        id: 11,
-        enterprise: "STM",
-        type: "Expense",
-        category: "Transport",
-    },
-    {
-        id: 12,
-        enterprise: "Nissan",
-        type: "Expense",
-        category: "Automobile",
-    },
-    {
-        id: 13,
-        enterprise: "Cinema Guzzo",
-        type: "Expense",
-        category: "Entertainment",
-    },
-    {
-        id: 14,
-        enterprise: "Maxi",
-        type: "Expense",
-        category: "Grocery",
-    },
-    {
-        id: 15,
-        enterprise: "Couche Tard",
-        type: "Expense",
-        category: "Grocery",
-    },
-    {
-        id: 16,
-        enterprise: "Pizza Pizza",
-        type: "Expense",
-        category: "Restaurant",
-    },
-    {
-        id: 17,
-        enterprise: "Pizza Hut",
-        type: "Expense",
-        category: "Restaurant",
-    },
-    {
-        id: 18,
-        enterprise: "Decathlon",
-        type: "Expense",
-        category: "Sport",
-    },
-    {
-        id: 19,
-        enterprise: "Forever 21",
-        type: "Expense",
-        category: "Fashion",
-    },
-    {
-        id: 20,
-        enterprise: "Amazon",
-        type: "Expense",
-        category: "Technology",
-    },
-    {
-        id: 21,
-        enterprise: "Newegg",
-        type: "Expense",
-        category: "Technology",
-    },
-    {
-        id: 22,
-        enterprise: "H & M",
-        type: "Expense",
-        category: "Fashion",
-    },
-    {
-        id: 23,
-        enterprise: "Wal-Mart",
-        type: "Expense",
-        category: "Grocery",
-    },
-    {
-        id: 24,
-        enterprise: "Garage S & M",
-        type: "Expense",
-        category: "Automobile",
-    }
-]
+	{
+		id: 1,
+		enterprise: "Tim Hortons",
+		type: "Expense",
+		category: "Restaurant",
+	},
+	{
+		id: 2,
+		enterprise: "McDonalds",
+		type: "Expense",
+		category: "Restaurant",
+	},
+	{
+		id: 3,
+		enterprise: "Burger King",
+		type: "Expense",
+		category: "Restaurant",
+	},
+	{
+		id: 4,
+		enterprise: "Netflix",
+		type: "Expense",
+		category: "Entertainment",
+	},
+	{
+		id: 5,
+		enterprise: "Metro",
+		type: "Expense",
+		category: "Grocery",
+	},
+	{
+		id: 6,
+		enterprise: "IGA",
+		type: "Expense",
+		category: "Grocery",
+	},
+	{
+		id: 7,
+		enterprise: "Pacini",
+		type: "Expense",
+		category: "Restaurant",
+	},
+	{
+		id: 8,
+		enterprise: "Cineplex Odeon",
+		type: "Expense",
+		category: "Entertainment",
+	},
+	{
+		id: 9,
+		enterprise: "Desjardins",
+		type: "Expense",
+		category: "Bank",
+	},
+	{
+		id: 10,
+		enterprise: "Services Conseils Franc-Jeu",
+		type: "Income",
+		category: "Revenue",
+	},
+	{
+		id: 11,
+		enterprise: "STM",
+		type: "Expense",
+		category: "Transport",
+	},
+	{
+		id: 12,
+		enterprise: "Nissan",
+		type: "Expense",
+		category: "Automobile",
+	},
+	{
+		id: 13,
+		enterprise: "Cinema Guzzo",
+		type: "Expense",
+		category: "Entertainment",
+	},
+	{
+		id: 14,
+		enterprise: "Maxi",
+		type: "Expense",
+		category: "Grocery",
+	},
+	{
+		id: 15,
+		enterprise: "Couche Tard",
+		type: "Expense",
+		category: "Grocery",
+	},
+	{
+		id: 16,
+		enterprise: "Pizza Pizza",
+		type: "Expense",
+		category: "Restaurant",
+	},
+	{
+		id: 17,
+		enterprise: "Pizza Hut",
+		type: "Expense",
+		category: "Restaurant",
+	},
+	{
+		id: 18,
+		enterprise: "Decathlon",
+		type: "Expense",
+		category: "Sport",
+	},
+	{
+		id: 19,
+		enterprise: "Forever 21",
+		type: "Expense",
+		category: "Fashion",
+	},
+	{
+		id: 20,
+		enterprise: "Amazon",
+		type: "Expense",
+		category: "Technology",
+	},
+	{
+		id: 21,
+		enterprise: "Newegg",
+		type: "Expense",
+		category: "Technology",
+	},
+	{
+		id: 22,
+		enterprise: "H & M",
+		type: "Expense",
+		category: "Fashion",
+	},
+	{
+		id: 23,
+		enterprise: "Wal-Mart",
+		type: "Expense",
+		category: "Grocery",
+	},
+	{
+		id: 24,
+		enterprise: "Garage S & M",
+		type: "Expense",
+		category: "Automobile",
+	}
+];
 
 export const dummyTransactions = [
-    {
-        id: 1,
-        date: "2018-10-11",
-        enterprise: "Tim Hortons",
-        type: "Expense",
-        category: "Restaurant",
-        total: 1000.24
-    },
-    {
-        id: 2,
-        date: "2018-09-11",
-        enterprise: "McDonalds",
-        type: "Expense",
-        category: "Restaurant",
-        total: 1000.24
-    },
-    {
-        id: 3,
-        date: "2018-08-11",
-        enterprise: "Burger King",
-        type: "Expense",
-        category: "Restaurant",
-        total: 1000.24
-    },
-    {
-        id: 4,
-        date: "2018-10-11",
-        enterprise: "Netflix",
-        type: "Expense",
-        category: "Entertainment",
-        total: 1000.24
-    },
-    {
-        id: 5,
-        date: "2018-10-11",
-        enterprise: "Metro",
-        type: "Expense",
-        category: "Grocery",
-        total: 1000.24
-    },
-    {
-        id: 6,
-        date: "2018-10-11",
-        enterprise: "IGA",
-        type: "Expense",
-        category: "Grocery",
-        total: 1000.24
-    }
-]
+	{
+		id: 1,
+		date: "2020-02-08",
+		enterprise: "Tim Hortons",
+		type: "Expense",
+		category: "Restaurant",
+		total: 1000.24
+	},
+	{
+		id: 2,
+		date: "2020-02-08",
+		enterprise: "McDonalds",
+		type: "Expense",
+		category: "Restaurant",
+		total: 1000.24
+	},
+	{
+		id: 3,
+		date: "2020-01-30",
+		enterprise: "Burger King",
+		type: "Expense",
+		category: "Restaurant",
+		total: 1000.24
+	},
+	{
+		id: 4,
+		date: "2020-01-15",
+		enterprise: "Netflix",
+		type: "Expense",
+		category: "Entertainment",
+		total: 1000.24
+	},
+	{
+		id: 5,
+		date: "2020-02-08",
+		enterprise: "Metro",
+		type: "Expense",
+		category: "Grocery",
+		total: 1000.24
+	},
+	{
+		id: 6,
+		date: "2020-02-01",
+		enterprise: "IGA",
+		type: "Expense",
+		category: "Grocery",
+		total: 1000.24
+	}
+];
 
 export const dummyCategories = [
-    { id: 1, title: "Entertainment" },
-    { id: 2, title: "Technology" },
-    { id: 3, title: "Grocery" },
-    { id: 4, title: "Fashion" },
-    { id: 5, title: "Automobile" },
-    { id: 6, title: "Automobile" },
-    { id: 7, title: "Automobile" },
-    { id: 8, title: "Automobile" },
-    { id: 9, title: "Automobile" },
-    { id: 10, title: "Automobile" },
-    { id: 11, title: "Automobile" },
-    { id: 12, title: "Automobile" },
-    { id: 13, title: "Automobile" },
-    { id: 14, title: "Automobile" },
-    { id: 15, title: "Automobile" },
-    { id: 16, title: "Automobile" }
-]
+	{ id: 1, title: "Entertainment" },
+	{ id: 2, title: "Technology" },
+	{ id: 3, title: "Grocery" },
+	{ id: 4, title: "Fashion" },
+	{ id: 5, title: "Automobile" },
+	{ id: 6, title: "Automobile" },
+	{ id: 7, title: "Automobile" },
+	{ id: 8, title: "Automobile" },
+	{ id: 9, title: "Automobile" },
+	{ id: 10, title: "Automobile" },
+	{ id: 11, title: "Automobile" },
+	{ id: 12, title: "Automobile" },
+	{ id: 13, title: "Automobile" },
+	{ id: 14, title: "Automobile" },
+	{ id: 15, title: "Automobile" },
+	{ id: 16, title: "Automobile" }
+];
