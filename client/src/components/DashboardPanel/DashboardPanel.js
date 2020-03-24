d0c7a96fbf815add7fb7f6f4a32c8a1e5049959d
Jean-Frederic Mainville <jfmainville@outlook.com>
2020-03-23 21:11:58 -0400

    Update the DashboardPanel component
    Update the DashboardPanel component to make it easier to manage by separating the chart components into multiple ones.
    
    - Import the moment module
    - Add the accounts and transactions states
    - Add the activeYear state
    - Add the useEffect method to extract the accounts and transactions
    - Add the MonthlyTransactionChart.js component
    - Add the TotalTransactionChart.js component

--- client/src/components/DashboardPanel/DashboardPanel.js
+++ client/src/components/DashboardPanel/DashboardPanel.js
@@ -1,71 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./DashboardPanel.module.scss";
-import { Bar, Doughnut } from "react-chartjs-2";
+import { useDispatch, useSelector } from "react-redux";
+import MonthlyTransactionChart from "./MonthlyTransactionChart/MonthlyTransactionChart";
+import * as actions from "../../store/actions";
+import TotalTransactionChart from "./TotalTransactionChart/TotalTransactionChart";
+import moment from "moment";
 
 const DashboardPanel = () => {
-	const monthlyIncomesExpensesData = {
-		labels: [
-			"Jan",
-			"Feb",
-			"Mar",
-			"Apr",
-			"May",
-			"Jun",
-			"Jul",
-			"Aug",
-			"Sep",
-			"Oct",
-			"Nov",
-			"Dec"
-		],
-		datasets: [
-			{
-				label: "Incomes",
-				data: [
-					1000,
-					1200,
-					1500,
-					1500,
-					1000,
-					2000,
-					1300,
-					1200,
-					1100,
-					1033,
-					1200,
-					1300
-				],
-				backgroundColor: "#29a529"
-			},
-			{
-				label: "Expenses",
-				data: [
-					1000,
-					1200,
-					700,
-					700,
-					1000,
-					1200,
-					1300,
-					1200,
-					1100,
-					1000,
-					1400,
-					1500
-				],
-				backgroundColor: "#c3323d"
-			}
-		]
-	};
-	const totalIncomesExpensesData = {
-		labels: ["Incomes", "Expenses"],
-		datasets: [
-			{
-				data: [13000, 11000],
-				backgroundColor: ["#29a529", "#c3323d"]
-			}
-		]
-	};
+	const accounts = useSelector(state => state.account.accounts);
+	const transactions = useSelector(state => state.transaction.transactions);
+	const [activeYear, setActiveYear] = useState(moment());
 	const categoryExpensesData = {
 		labels: [
 			"Automobile",
@@ -105,35 +49,25 @@ const DashboardPanel = () => {
 			]
 		}
 	};
-	const doughnutOptions = {
-		maintainAspectRatio: false,
-		responsive: true,
-		scales: {
-			xAxes: [
-				{
-					display: false,
-					gridLines: {
-						display: false
-					}
-				}
-			]
-		}
-	};
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(actions.fetchAccounts());
+		dispatch(actions.fetchTransactions());
+	}, []);
+
 	return (
 		<div className={classes.Container}>
 			<div className={classes.TopChart}>
-				<div className={classes.IncomesExpensesChart}>
-					<Bar
-						data={monthlyIncomesExpensesData}
-						options={options}
-						width={1400}
-						height={400}
-					/>
-				</div>
+				<MonthlyTransactionChart
+					transactions={transactions}
+					options={options}
+				/>
 			</div>
 			<div className={classes.BottomCharts}>
 				<TotalTransactionChart
 					transactions={transactions}
+					activeYear={activeYear}
 				/>
 			</div>
 		</div>
