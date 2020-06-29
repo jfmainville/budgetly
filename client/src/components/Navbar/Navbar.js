1a050d0858586664065e77312284c76fd17bde78
Jean-Frederic Mainville <jfmainville@outlook.com>
2020-06-29 19:39:43 -0400

    Refactor the Navbar overall styling.
    Refactor the Navbar overall styling to fix the resizing issue when reducing the size of the display.

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -6,40 +6,38 @@ import { NavLink } from "react-router-dom";
 const navbar = props => {
 	const [profileName] = useState("Jean-Frederic Mainville");
 	return (
-		<div className={classes.Navigation}>
+		<React.Fragment>
 			<div className={classes.Logo}>Budgetly</div>
-			<div className={classes.NavigationItems}>
-				<ul>
-					<li className={classes.NavigationItem}>
-						<NavLink
-							activeClassName={classes.NavigationItemLinkActive}
-							className={classes.NavigationItemLink}
-							to={"/"}
-							exact={true}
-						>
-							Dashboard
-						</NavLink>
-					</li>
-					<li className={classes.NavigationItem}>
-						<NavLink
-							activeClassName={classes.NavigationItemLinkActive}
-							className={classes.NavigationItemLink}
-							to={"/transactions"}
-						>
-							Transactions
-						</NavLink>
-					</li>
-					<li className={classes.NavigationItem}>
-						<NavLink
-							activeClassName={classes.NavigationItemLinkActive}
-							className={classes.NavigationItemLink}
-							to={"/accounts"}
-						>
-							Accounts
-						</NavLink>
-					</li>
-				</ul>
-			</div>
+			<ul>
+				<li className={classes.NavigationItem}>
+					<NavLink
+						activeClassName={classes.NavigationItemLinkActive}
+						className={classes.NavigationItemLink}
+						to={"/"}
+						exact={true}
+					>
+						Dashboard
+					</NavLink>
+				</li>
+				<li className={classes.NavigationItem}>
+					<NavLink
+						activeClassName={classes.NavigationItemLinkActive}
+						className={classes.NavigationItemLink}
+						to={"/transactions"}
+					>
+						Transactions
+					</NavLink>
+				</li>
+				<li className={classes.NavigationItem}>
+					<NavLink
+						activeClassName={classes.NavigationItemLinkActive}
+						className={classes.NavigationItemLink}
+						to={"/accounts"}
+					>
+						Accounts
+					</NavLink>
+				</li>
+			</ul>
 			<div className={classes.UserNavigation}>
 				<div className={classes.Profile}>
 					<p className={classes.ProfileName}>{profileName}</p>
@@ -47,12 +45,12 @@ const navbar = props => {
 				<div className={classes.Settings}>
 					<button className={classes.SettingsButton}>
 						<svg className={classes.SettingsIcon}>
-							<use xlinkHref="/assets/sprite.svg#icon-cog" />
+							<use xlinkHref="/assets/sprite.svg#icon-cog"/>
 						</svg>
 					</button>
 				</div>
 			</div>
-		</div>
+		</React.Fragment>
 	);
 };
 
