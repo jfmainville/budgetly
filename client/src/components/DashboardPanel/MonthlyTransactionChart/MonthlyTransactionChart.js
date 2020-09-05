50dd72bc79bb663cf0eb62e843e6db0d813f4e98
Jean-Frederic Mainville <jfmainville@outlook.com>
2020-09-05 12:51:25 -0400

    Add the MonthlyTransactionChart component
    Add the MonthlyTransactionChart component to the project.

---
+++ client/src/components/DashboardPanel/MonthlyTransactionChart/MonthlyTransactionChart.js
@@ -0,0 +1,91 @@
+import React from "react";
+import classes from "./MonthlyTransactionChart.module.scss";
+import { Bar } from "react-chartjs-2";
+import moment from "moment";
+
+const monthlyTransactionChart = props => {
+	const transactions = props.transactions;
+	const months = [
+		"Jan",
+		"Feb",
+		"Mar",
+		"Apr",
+		"May",
+		"Jun",
+		"Jul",
+		"Aug",
+		"Sep",
+		"Oct",
+		"Nov",
+		"Dec"
+	];
+	let filteredTransactions = [];
+	let totalFilteredTransactions = [];
+	if (transactions) {
+		filteredTransactions = months.map(month => transactions.filter(
+			transaction =>
+				moment(transaction.date, "YYYY-MM-DD").format("YYYY-MM") === moment().month(month).format("YYYY-MM")
+		));
+		if (filteredTransactions.length > 0) {
+			totalFilteredTransactions = filteredTransactions
+				.map(transaction => transaction.total)
+				.reduce((accumulator, currentValue) => accumulator + currentValue);
+		}
+	}
+	const monthlyTransactionsData = {
+		labels: months,
+		datasets: [
+			{
+				label: "Incomes",
+				data: [
+					1000,
+					1200,
+					1500,
+					1500,
+					1000,
+					2000,
+					1300,
+					1200,
+					1100,
+					1033,
+					1200,
+					1300
+				],
+				backgroundColor: "#29a529"
+			},
+			{
+				label: "Expenses",
+				data: [
+					1000,
+					1200,
+					700,
+					700,
+					1000,
+					1200,
+					1300,
+					1200,
+					1100,
+					1000,
+					1400,
+					1500
+				],
+				backgroundColor: "#c3323d"
+			}
+		]
+	};
+
+	return (
+		<div className={classes.TopChart}>
+			<div className={classes.IncomesExpensesChart}>
+				<Bar
+					data={monthlyTransactionsData}
+					options={props.options}
+					width={1000}
+					height={400}
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default monthlyTransactionChart;
