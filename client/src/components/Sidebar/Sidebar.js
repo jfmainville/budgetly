c2474ea6abec31c094e69bf659c6adc9fc628135
Jean-Frederic Mainville <jfmainville@outlook.com>
2020-10-30 14:45:22 -0400

    Update Sidebar.js component with the new SVG icons
    Update the Sidebar.js component with the new SVG icons.

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -14,7 +14,7 @@ const sidebar = props => {
 						exact={true}
 					>
 						<svg className={classes.Icon}>
-							<use xlinkHref="/assets/sprite.svg#icon-stats-bars"/>
+							<use xlinkHref="/assets/sprite.svg#icon-bar-graph"/>
 						</svg>
 						Dashboard
 					</NavLink>
@@ -26,7 +26,7 @@ const sidebar = props => {
 						to={"/transactions"}
 					>
 						<svg className={classes.Icon}>
-							<use xlinkHref="/assets/sprite.svg#icon-file-text"/>
+							<use xlinkHref="/assets/sprite.svg#icon-text-document"/>
 						</svg>
 						Transactions
 					</NavLink>
@@ -38,7 +38,7 @@ const sidebar = props => {
 						to={"/accounts"}
 					>
 						<svg className={classes.Icon}>
-							<use xlinkHref="/assets/sprite.svg#icon-credit-card"/>
+							<use xlinkHref="/assets/sprite.svg#icon-wallet"/>
 						</svg>
 						Accounts
 					</NavLink>
