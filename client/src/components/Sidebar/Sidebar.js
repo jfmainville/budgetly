ed279d116cc4b5e345e38ebb6c716f5b29434afc
Jean-Frederic Mainville <jfmainville@outlook.com>
2020-12-22 11:02:42 -0500

    Fix TransactionDatePicker.js component display issue
    Fix the TransactionDatePicker.js component display issue.

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -2,49 +2,47 @@ import React from "react";
 import classes from "./Sidebar.module.scss";
 import { NavLink } from "react-router-dom";
 
-const sidebar = props => {
+const sidebar = () => {
 	return (
-		<React.Fragment>
-			<ul>
-				<li className={classes.NavigationItem}>
-					<NavLink
-						activeClassName={classes.NavigationItemLinkActive}
-						className={classes.NavigationItemLink}
-						to={"/"}
-						exact={true}
-					>
-						<svg className={classes.Icon}>
-							<use xlinkHref="/assets/sprite.svg#icon-bar-graph"/>
-						</svg>
-						Dashboard
-					</NavLink>
-				</li>
-				<li className={classes.NavigationItem}>
-					<NavLink
-						activeClassName={classes.NavigationItemLinkActive}
-						className={classes.NavigationItemLink}
-						to={"/transactions"}
-					>
-						<svg className={classes.Icon}>
-							<use xlinkHref="/assets/sprite.svg#icon-text-document"/>
-						</svg>
-						Transactions
-					</NavLink>
-				</li>
-				<li className={classes.NavigationItem}>
-					<NavLink
-						activeClassName={classes.NavigationItemLinkActive}
-						className={classes.NavigationItemLink}
-						to={"/accounts"}
-					>
-						<svg className={classes.Icon}>
-							<use xlinkHref="/assets/sprite.svg#icon-wallet"/>
-						</svg>
-						Accounts
-					</NavLink>
-				</li>
-			</ul>
-		</React.Fragment>
+		<div className={classes.Sidebar}>
+			<li className={classes.NavigationItem}>
+				<NavLink
+					activeClassName={classes.NavigationItemLinkActive}
+					className={classes.NavigationItemLink}
+					to={"/"}
+					exact={true}
+				>
+					<svg className={classes.Icon}>
+						<use xlinkHref="/assets/sprite.svg#icon-bar-graph"/>
+					</svg>
+					Dashboard
+				</NavLink>
+			</li>
+			<li className={classes.NavigationItem}>
+				<NavLink
+					activeClassName={classes.NavigationItemLinkActive}
+					className={classes.NavigationItemLink}
+					to={"/transactions"}
+				>
+					<svg className={classes.Icon}>
+						<use xlinkHref="/assets/sprite.svg#icon-text-document"/>
+					</svg>
+					Transactions
+				</NavLink>
+			</li>
+			<li className={classes.NavigationItem}>
+				<NavLink
+					activeClassName={classes.NavigationItemLinkActive}
+					className={classes.NavigationItemLink}
+					to={"/accounts"}
+				>
+					<svg className={classes.Icon}>
+						<use xlinkHref="/assets/sprite.svg#icon-wallet"/>
+					</svg>
+					Accounts
+				</NavLink>
+			</li>
+		</div>
 	);
 };
 
